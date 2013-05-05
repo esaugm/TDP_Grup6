afb4262b7daec5927a3af4f3270138ec579b6c08
Mikodes <miguel.lopez.hi@gmail.com>
2013-05-05 08:40:01 +0200

    created Blank Generic Dao

---
+++ pac4/server/src/main/java/common/dao/GenericDao.java
@@ -0,0 +1,11 @@
+package common.dao;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mike
+ * Date: 05/05/13
+ * Time: 08:38
+ * To change this template use File | Settings | File Templates.
+ */
+public interface GenericDao {
+}
---
+++ pac4/server/src/main/java/common/dao/impl/GenericDaoImpl.java
@@ -0,0 +1,13 @@
+package common.dao.impl;
+
+import common.dao.GenericDao;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mike
+ * Date: 05/05/13
+ * Time: 08:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class GenericDaoImpl implements GenericDao {
+}
