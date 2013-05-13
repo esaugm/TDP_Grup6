051b6434358d533c269625853bb8c22535ba30dd
jiquintana <joseignacio.quintana@gmail.com>
2013-05-13 08:39:01 +0200

    Artefactos para poder hacer test en Solicitud Este cambio es temporal. Josi (jiquintana@uoc.edu) Signed-off-by:jiquintana <joseignacio.quintana@gmail.com>

--- pac4/server/src/main/resources/sql/DDM_changes.sql
+++ pac4/server/src/main/resources/sql/DDM_changes.sql
@@ -8,26 +8,30 @@
 -- * @author jiquintana (jiquintana@uoc.edu)
 -- */
 
-INSERT INTO taller
-(id, cif, adreca, capacitat, captaller, telefon, web, actiu, dataapertura, datamodificacio, databaixa)
-VALUES (-1, '00000000', 'vacio', 1, -1, '00000000', 'vacio', false, '2013-05-12', NULL, NULL);
+BEGIN WORK;
 
-INSERT INTO usuari
-(id, taller, usuari, perfil, contrasenya, actiu, dataalta, datamodificacio, databaixa, reparacionsassignades)
+SET CONSTRAINTS ALL DEFERRED;
+alter table usuari disable trigger all;
+
+INSERT INTO usuari (id, taller, usuari, perfil, contrasenya, actiu, dataalta, datamodificacio, databaixa, reparacionsassignades)
 VALUES (-1, -1, 'vacio', 'vacio', 'NULL', false, '2013-05-12', NULL, NULL, 0);
 
 INSERT INTO captaller
 (idcaptaller)
 VALUES (-1);
 
-INSERT INTO mecanic
-(idmecanic, disponible, idrep1, idrep2)
+INSERT INTO taller (id, cif, adreca, capacitat, captaller, telefon, web, actiu, dataapertura, datamodificacio, databaixa)
+VALUES (-1, '00000000', 'vacio', 1, -1, '00000000', 'vacio', false, '2013-05-12', NULL, NULL);
+
+INSERT INTO mecanic (idmecanic, disponible, idrep1, idrep2)
 VALUES (-1, false, -1, -1);
 
-INSERT INTO reparacio
-(ordrereparacio, idcaptaller, acceptada, idmecanic, assignada, comptador, observacions, numcom, dataassignacio, datainici, datafi)
+INSERT INTO reparacio (ordrereparacio, idcaptaller, acceptada, idmecanic, assignada, comptador, observacions, numcom, dataassignacio, datainici, datafi)
 VALUES (-1, -1, false, -1, false, 0, 'vacio', -1, NULL, NULL, NULL);
 
-INSERT INTO asseguradora
-(idasseguradora, cif, nom, adreca, telefon)
+INSERT INTO asseguradora (idasseguradora, cif, nom, adreca, telefon)
 VALUES (-1, '000000000', 'vacio', 'vacio', 0);
+
+alter table usuari enable trigger all;
+
+COMMIT;
